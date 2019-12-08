8de70b0f2d64d50bbf59a492e2c57de1246de5d3
Felipe Elia <felipe.elia@gmail.com>
2019-12-08 18:10:56 -0300

    Check if ESLint necessary packages are installed

--- actions/eslint/entrypoint.sh
+++ actions/eslint/entrypoint.sh
@@ -6,4 +6,37 @@ cd "$GITHUB_WORKSPACE"
 
 npm install
 
+if [ "$(npm list eslint | grep empty)" ]; then
+	echo 'Installing ESLint'
+    npm install eslint
+fi
+
+if [ "$(npm list @wordpress/eslint-plugin | grep empty)" ]; then
+	echo 'Installing WordPress Coding Standard'
+    npm install @wordpress/eslint-plugin
+fi
+
+if [ -f .eslintrc ] || [ -f .eslintrc.json ] || [ -f .eslintrc.yaml ] || [ -f .eslintrc.yml ] || [ -f .eslintrc.js ]
+then
+	echo 'Creating ESLint config file'
+	cat << ESLINT > ./.stylelintrc
+{
+  "extends": [
+    "plugin:@wordpress/eslint-plugin/recommended"
+  ],
+  "root": true,
+  "env": {
+    "browser": true,
+    "node": true,
+    "jquery": true
+  },
+  "globals": {
+    "wp": true,
+    "wpu": true
+  }
+}
+ESLINT
+fi
+
+echo 'Running ESLint'
 npx eslint **/*.js

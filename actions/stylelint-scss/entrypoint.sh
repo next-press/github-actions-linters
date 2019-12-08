8bd432f60304efff63313ee698a793b404cb6d61
Felipe Elia <felipe.elia@gmail.com>
2019-12-08 18:08:01 -0300

    Check if stylelint necessary packages are installed

--- actions/stylelint-scss/entrypoint.sh
+++ actions/stylelint-scss/entrypoint.sh
@@ -6,4 +6,25 @@ cd "$GITHUB_WORKSPACE"
 
 npm install
 
+if [ "$(npm list stylelint | grep empty)" ]; then
+	echo 'Installing stylelint'
+    npm install stylelint
+fi
+
+if [ "$(npm list stylelint-config-wordpress | grep empty)" ]; then
+	echo 'Installing WordPress Coding Standard'
+    npm install stylelint-config-wordpress
+fi
+
+if [ -f .stylelintrc ] || [ -f .stylelintrc.json ] || [ -f .stylelintrc.yaml ] || [ -f .stylelintrc.yml ] || [ -f .stylelintrc.js ]
+then
+	echo 'Creating stylelint config file'
+	cat << STYLELINT > ./.stylelintrc
+{
+  "extends": "stylelint-config-wordpress/scss"
+}
+STYLELINT
+fi
+
+echo 'Running stylelint'
 npx stylelint **/*.scss --allow-empty-input

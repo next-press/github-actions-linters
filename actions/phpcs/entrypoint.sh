652ade4f15d7a7486a955bb8594a87c55209904e
Felipe Elia <felipe.elia@gmail.com>
2019-12-08 16:48:03 -0300

    PHPCS should lint only PHP files

--- actions/phpcs/entrypoint.sh
+++ actions/phpcs/entrypoint.sh
@@ -24,7 +24,7 @@ else
     composer require dealerdirect/phpcodesniffer-composer-installer wp-coding-standards/wpcs
 fi
 
-STANDARD=' --standard=WordPress'
+STANDARD=' --standard=WordPress --extensions=php'
 if [ -f .phpcs.xml ] || [ -f phpcs.xml ] || [ -f .phpcs.xml.dist ] || [ -f phpcs.xml.dist ]
 then
 	STANDARD=''

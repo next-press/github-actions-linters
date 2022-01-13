6734a143ed2b72b7310e4172e461eef702186f13
Arindo Duque <aanduque@users.noreply.github.com>
2022-01-13 14:33:12 -0300

    Update entrypoint.sh

--- actions/phpcs/entrypoint.sh
+++ actions/phpcs/entrypoint.sh
@@ -6,9 +6,9 @@ cd "$GITHUB_WORKSPACE"
 
 # Use the name node_modules as it is usually already excluded
 # on the phpcs.xml file.
-echo 'Create sub-folder, to avoid conflicts.'
-mkdir node_modules
-cd node_modules
+# echo 'Create sub-folder, to avoid conflicts.'
+# mkdir node_modules
+# cd node_modules
 
 if [ ! -f composer.json ]; then
     echo 'Setting up a Composer file'
@@ -27,7 +27,7 @@ if phpcs_package_exist; then
 else
     echo 'PHP_CodeSniffer is not installed'
     echo 'Installing PHP_CodeSniffer and WordPress CS'
-    composer require dealerdirect/phpcodesniffer-composer-installer wp-coding-standards/wpcs
+    composer require dealerdirect/phpcodesniffer-composer-installer:0.5.0 wp-coding-standards/wpcs
 fi
 
 echo 'Installing NextPress Coding Standards'
